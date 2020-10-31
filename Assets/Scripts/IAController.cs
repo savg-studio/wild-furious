a71817017a9e187374d55ba77efe1a40845b56d4
Pablojea <pablo_ojea@outlook.com>
2020-10-31 15:05:13 +0100

    added IA basic movement script

---
+++ Assets/Scripts/IAController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAController : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 translation = new Vector3(speed * Time.deltaTime, 0, 0);
+        this.transform.Translate(translation);
+    }
+}
