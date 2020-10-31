ea386af443288510abe2e240ecc1e0e1535c3605
Xoán <xoancosmed@gmail.com>
2020-10-31 20:39:31 +0100

    Added unit tests and changed build configuration

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -6,13 +6,13 @@ using Random = UnityEngine.Random;
 
 public class SpawnController : MonoBehaviour
 {
-    [SerializeField] Transform spawnableZone = null;
-    [SerializeField] List<SpawnableObject> spawnableObjects = null;
+    [SerializeField] public Transform spawnableZone = null;
+    [SerializeField] public List<SpawnableObject> spawnableObjects = null;
 
     void Start()
     {
         if (spawnableZone == null || spawnableObjects == null) return;
-
+        
         float minX = spawnableZone.position.x - (spawnableZone.lossyScale.x / 2);
         float maxX = spawnableZone.position.x + (spawnableZone.lossyScale.x / 2);
         float minY = spawnableZone.position.y - (spawnableZone.lossyScale.y / 2);
---
+++ Assets/Testing/PlayTests/SpawnTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace RacingTests
+{
+    public class SpawnTests : MonoBehaviour
+    {
+        private const string TEMPLATE_1_NAME = "TEMPLATE_1";
+        private const string TEMPLATE_2_NAME = "TEMPLATE_2";
+        private const int TEMPLATE_1_NUM = 1;
+        private const int TEMPLATE_2_NUM = 4;
+
+        private GameObject spawner;
+        private SpawnController spawnCtrl;
+
+        [UnityTest]
+        public IEnumerator ShouldSpawnObjects()
+        {
+            // SPAWN OBJECTS
+
+            SpawnController.SpawnableObject template1 = new SpawnController.SpawnableObject();
+            template1.prefab = new GameObject(TEMPLATE_1_NAME);
+            template1.instances = TEMPLATE_1_NUM;
+            SpawnController.SpawnableObject template2 = new SpawnController.SpawnableObject();
+            template2.prefab = new GameObject(TEMPLATE_2_NAME);
+            template2.instances = TEMPLATE_2_NUM;
+
+            GameObject zone = new GameObject();
+            zone.transform.localScale = new Vector3(1000, 1000, 1);
+
+            spawner = new GameObject("Spawner");
+            spawner.SetActive(false);
+            spawnCtrl = spawner.AddComponent<SpawnController>();
+            spawnCtrl.spawnableZone = zone.transform;
+            spawnCtrl.spawnableObjects = new List<SpawnController.SpawnableObject>();
+            spawnCtrl.spawnableObjects.Add(template1);
+            spawnCtrl.spawnableObjects.Add(template2);
+            spawner.SetActive(true);
+
+            // NEXT FRAME
+
+            yield return null;
+
+            // CHECK IF OBJECTS WERE SPAWNED
+
+            int existing1 = 0;
+            int existing2 = 0;
+            foreach (GameObject obj in FindObjectsOfType(typeof(GameObject)))
+            {
+                if (obj.name.Contains(TEMPLATE_1_NAME)) existing1++;
+                if (obj.name.Contains(TEMPLATE_2_NAME)) existing2++;
+            }
+
+            Assert.AreEqual(TEMPLATE_1_NUM + 1, existing1);
+            Assert.AreEqual(TEMPLATE_2_NUM + 1, existing2);
+        }
+    }
+}
