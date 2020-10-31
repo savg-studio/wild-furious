a70458f675c064d2b0aee84755104093d219b667
Pablojea <pablo_ojea@outlook.com>
2020-10-31 15:17:50 +0100

    added Camera Tracking script

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking: MonoBehaviour
+{
+
+    public Vector3 cameraOffset = new Vector3(0.0f, 1.3f, -3.0f);
+    private Transform target;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+
+    }
+
+    // Update is called once per frame
+
+
+    private void LateUpdate()
+    {       
+            this.transform.position = target.TransformPoint(cameraOffset);
+            this.transform.LookAt(target);
+       
+    }
+}
