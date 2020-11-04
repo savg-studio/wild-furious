88cce52655b7c05ad38bb8cc24629c1cf4d2329f
Pablojea <pablo_ojea@outlook.com>
2020-11-04 18:29:54 +0100

    speed now increases gradually

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+    public float speed = 1.0f;
+    public float verticalSpeed = 8.0f;
+    public float maxSpeed = 20;
     private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
+    public bool dashAvailable = true;
     
     // Start is called before the first frame update
     void Start()
@@ -17,26 +20,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //S = V*t;
-        if (Mathf.Abs(Input.GetAxisRaw(AXIS_H)) > 0.2f)
-        {
-            Vector3 translation = new Vector3(Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
-            this.transform.Translate(translation);
+        Movement();
+        
 
-        }
+    }
+    private void FixedUpdate()
+    {
+        SpeedAceleration();
+
+    }
+
+    public void Movement()
+    {
+
+        Vector3 translation = new Vector3(speed * Time.deltaTime, 0, 0);
+        this.transform.Translate(translation);
+
 
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
         {
-            Vector3 translation = new Vector3(0, Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
-            this.transform.Translate(translation);
-
+            Vector3 translation2 = new Vector3(0, Input.GetAxisRaw(AXIS_V) * verticalSpeed * Time.deltaTime, 0);
+            this.transform.Translate(translation2);
 
         }
     }
 
-   public void speedModifier(float mod)
+    public bool Dash(bool dashAvailable)
     {
-        speed *= mod;
+
+        return false;
+    }
+
+    public void SpeedAceleration()
+    {
+        if(speed<=maxSpeed)
+        speed += 0.02f;
     }
 
 }
