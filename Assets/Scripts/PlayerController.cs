ab6c46bec66d65e2a287a88738b0386623eb1c20
Pablojea <pablo_ojea@outlook.com>
2020-11-05 12:46:35 +0100

    implemented upper dash mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    //RB
+    Rigidbody2D carRb;
+    //movement variables
     public float speed = 1.0f;
     public float verticalSpeed = 8.0f;
-    public float maxSpeed = 20;
+    public float maxSpeed = 16;
+
+    //dash variables
+    public float dashTimeGap = 0.3f;
+    public float dashCoolDown = 5.0f;
+    public bool dashAvailable = true;    
+    public float _startDashTime=0.1f;
+
+    public float dashSpeed = 20;
+    private bool dashing = false;
+    private float _dashPressedTime; //time when dash button was pressed last
+    private float _lastDashTime = 0; //time when dash was last used
+    private float _dashTime;
+
+
+    //input variables
     private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
-    public bool dashAvailable = true;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        carRb = this.GetComponent<Rigidbody2D>();
+        _dashPressedTime = Time.time;
+        _dashTime = _startDashTime;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        DashCooldown();
         Movement();
-        
-
+        DashManagement();
     }
+
     private void FixedUpdate()
     {
         SpeedAceleration();
 
+        if (dashing)
+        {
+            Dashing();
+        }
     }
 
     public void Movement()
@@ -41,14 +66,54 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 translation2 = new Vector3(0, Input.GetAxisRaw(AXIS_V) * verticalSpeed * Time.deltaTime, 0);
             this.transform.Translate(translation2);
-
         }
     }
 
-    public bool Dash(bool dashAvailable)
-    {
+    public void DashManagement()
+    {   
 
-        return false;
+        if (dashAvailable)
+        {
+            
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                float timeSinceLastPress = Time.time - _dashPressedTime;
+                
+                if(timeSinceLastPress <= dashTimeGap)
+                {                   
+                    Debug.Log("dash");
+                    dashing = true;
+                    _lastDashTime = Time.time;
+                }
+                else
+                {                  
+                    Debug.Log("No dash");
+                }
+                _dashPressedTime = Time.time;
+            }
+        }        
+    }
+
+    public void Dashing()
+    {
+        if (_dashTime > 0.1)
+        {
+            _dashTime -= Time.deltaTime;
+            carRb.velocity = Vector2.up * dashSpeed;
+
+        }
+        else
+        {
+            carRb.velocity = Vector2.up * 0;
+            dashing = false;
+            _dashTime = _startDashTime;
+        }
+
+    }
+
+    public void DashCooldown()
+    {
+        dashAvailable = (Time.time - _lastDashTime) > dashCoolDown;
     }
 
     public void SpeedAceleration()
