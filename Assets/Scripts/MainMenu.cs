906229c72a931b464cf2be312537d90fbf2cc95e
Xoán <xoancosmed@gmail.com>
2020-11-06 22:18:40 +0100

    Play and exit buttons

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -9,14 +10,29 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Text marioCarTitle;
     [SerializeField] private Text wildAndFuriousTitle;
     [SerializeField] private Color textColor = new Color();
+
+    [SerializeField] private Text playButtonText;
+    [SerializeField] private Text exitButtonText;
+    [SerializeField] private Image playButtonImage;
+    [SerializeField] private Image exitButtonImage;
+    [SerializeField] private Color buttonTextColor = new Color();
+    [SerializeField] private Color buttonImageColor = new Color();
+
     //[SerializeField] private float fadeDuration = 1;
 
     void Start()
     {
-        Color initialColor = GetAlphaColor(textColor, 0);
-        savgsTitle.color = initialColor;
-        marioCarTitle.color = initialColor;
-        wildAndFuriousTitle.color = initialColor;
+        Color initialTextColor = GetAlphaColor(textColor, 0);
+        savgsTitle.color = initialTextColor;
+        marioCarTitle.color = initialTextColor;
+        wildAndFuriousTitle.color = initialTextColor;
+
+        Color initialButtonTextColor = GetAlphaColor(buttonTextColor, 0);
+        playButtonText.color = initialButtonTextColor;
+        exitButtonText.color = initialButtonTextColor;
+        Color initialButtonImageColor = GetAlphaColor(buttonImageColor, 0);
+        playButtonImage.color = initialButtonImageColor;
+        exitButtonImage.color = initialButtonImageColor;
 
         StartCoroutine(AnimateTitles());
     }
@@ -28,6 +44,7 @@ public class MainMenu : MonoBehaviour
 
     private IEnumerator AnimateTitles()
     {
+        // FADE IN TITLES (ONE BY ONE)
         for (float i = 0; i <= 1; i += Time.deltaTime)
         {
             savgsTitle.color = GetAlphaColor(textColor, i);
@@ -43,10 +60,37 @@ public class MainMenu : MonoBehaviour
             wildAndFuriousTitle.color = GetAlphaColor(textColor, i);
             yield return null;
         }
+
+        // FADE IN BUTTONS (SIMULANEOUSLY)
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            playButtonText.color = GetAlphaColor(buttonTextColor, i);
+            exitButtonText.color = GetAlphaColor(buttonTextColor, i);
+            playButtonImage.color = GetAlphaColor(buttonImageColor, i);
+            exitButtonImage.color = GetAlphaColor(buttonImageColor, i);
+            yield return null;
+        }
     }
 
     private Color GetAlphaColor(Color color, float alpha)
     {
         return new Color(color.r, color.g, color.b, alpha);
     }
+
+    // BUTTON METHODS
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
 }
