1e0da8049b2db63c9767306ce5409afcbd2537c0
Xoán <xoancosmed@gmail.com>
2020-11-06 23:20:20 +0100

    Obstacles are now considered as power-ups that reduce the speed

---
+++ Assets/Scripts/Power-Ups/ObstaclePowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePowerUp : BasePowerUp
+{
+    [SerializeField] private int speedDivider = 4;
+
+    // POWER-UP FUNCTIONS
+
+    protected override void OnActivated(GameObject player)
+    {
+        PlayerController ctrl = player.GetComponent<PlayerController>();
+        if (ctrl != null)
+        {
+            ctrl.speed /= speedDivider;
+        }
+    }
+}
