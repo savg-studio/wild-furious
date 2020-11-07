b951c69b4967c65924004a3f98b9b542c5205c28
Xoán <xoancosmed@gmail.com>
2020-11-07 18:47:59 +0100

    UI for select menu

---
+++ Assets/Scripts/SelectMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectMenu : MonoBehaviour
+{
+    private string selectedCharacter = "NONE";
+
+    // BUTTON METHODS
+
+    public void SelectCharacter(string name)
+    {
+        selectedCharacter = name;
+    }
+
+    public void NormalMode()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SpecialMode()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
