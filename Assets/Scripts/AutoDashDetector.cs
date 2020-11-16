75c81f6f01db830373b08d4ce8958e7d0944cc0e
Xoán <xoancosmed@gmail.com>
2020-11-16 23:21:58 +0100

    Fix: fixed CPUs picking items with the dash trigger

---
+++ Assets/Scripts/AutoDashDetector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDashDetector : MonoBehaviour
+{
+    // Script for detecting other cars to make dash automatically
+    // This script should be placed in a child object o the CPU car (IAController) with a trigger collider
+    // For the parent object not detecting these collisions, this game object should have a knimatic rigidbody (https://answers.unity.com/questions/410711/trigger-in-child-object-calls-ontriggerenter-in-pa.html) (TODO: Improve this)
+
+    private IAController car = null;
+
+    void Start()
+    {
+        if (transform.parent != null)
+        {
+            car = transform.parent.GetComponent<IAController>();
+        }
+        if (car == null) Debug.LogError("Car cannot be null!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>() != null)
+        {
+            Transform posPlayer = collision.GetComponent<Transform>();
+            if (this.transform.position.y > posPlayer.transform.position.y)
+            {
+                car.dashAbajo = true;
+            }
+            else
+            {
+                car.dashArriba = true;
+            }
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (this.transform.position.y > -73)
+        {
+            car.dashAbajo = true;
+        }
+        else if (this.transform.position.y < -92)
+        {
+            car.dashArriba = true;
+        }
+    }
+}
--- Assets/Scripts/IAController.cs
+++ Assets/Scripts/IAController.cs
@@ -7,49 +7,9 @@ public class IAController : PlayerController
     public bool dashArriba = false;
     public bool dashAbajo = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         carRb = this.GetComponent<Rigidbody2D>();
-        //_dashPressedTime = Time.time;
-        //_dashTime = _startDashTime;
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        PowerUp powerUp = collision.GetComponent<PowerUp>();
-        if (powerUp != null) powerUp.OnCollected(gameObject);
-        else
-        {
-            Transform posPlayer = collision.GetComponent<Transform>();
-            if (this.transform.position.y > posPlayer.transform.position.y)
-            {
-                //Debug.Log("Dashear hacia abajo");
-                dashAbajo = true;
-            }
-            else
-            {
-                //Debug.Log("Dashear hacia arriba");
-                dashArriba = true;
-            }
-        }
-
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if(this.transform.position.y > -73)
-        {
-            dashAbajo = true;
-        }
-
-        if (this.transform.position.y < -92)
-        {
-            dashArriba = true;
-        }
-
-
     }
 
     public override void Movement()
